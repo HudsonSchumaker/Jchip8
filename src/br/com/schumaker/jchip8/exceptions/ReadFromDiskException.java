a2126c00a34b6cd2ea9c825458c009bab674e839
HudsonSchumaker <hudson@schumaker.com.br>
2020-11-07 04:58:50 +0100

    Check Style.

--- src/br/com/schumaker/jchip8/exceptions/ReadFromDiskException.java
+++ src/br/com/schumaker/jchip8/exceptions/ReadFromDiskException.java
@@ -2,9 +2,10 @@ package br.com.schumaker.jchip8.exceptions;
 
 /**
  *
- * @author hudson schumaker
+ * @author Hudson Schumaker
  */
 public class ReadFromDiskException  extends RuntimeException {
+    
     private static final long serialVersionUID = 1315284142012373060L;
     public static final String MESSAGE = "error.read.from.disk";
 
